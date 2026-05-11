7b012877d4168d81a40fe6a0134b018c863916ec
FURQAN K MULLANAVAR <mullafurqan89@gmail.com>
2026-05-11 13:27:21 +0530

    final3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD Pipeline Working Ganesh  hlo 🚀");
+  res.send("hlo maam 🚀");
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
